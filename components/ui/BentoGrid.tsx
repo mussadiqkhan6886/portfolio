d98c203acc26e80fa8e72f9f35954ca23711f81b
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-09-02 14:30:28 +0500

    completed first three cards

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,6 +1,8 @@
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 import { BackgroundGradientAnimation } from "./GradientBg";
+import { Globe } from "./Globe";
+import { GridGlobe } from "./GridGlobe";
 
 export const BentoGrid = ({
   className,
@@ -12,8 +14,8 @@ export const BentoGrid = ({
   return (
     <div
       className={cn(
-        "mx-auto grid max-w-7xl grid-cols-1 gap-4 md:auto-rows-[18rem] md:grid-cols-3",
-        className,
+        "grid grid-cols-1 md:grid-cols-6 lg:grid-cols-5 md:grid-row-7 gap-4 lg:gap-8 mx-auto",
+        className
       )}
     >
       {children}
@@ -43,8 +45,8 @@ export const BentoGridItem = ({
   return (
     <div
       className={cn(
-        "group/bento relative  shadow-input row-span-1 flex flex-col justify-between space-y-4 rounded-xl border-2 bg-white p-4 transition duration-200 hover:shadow-xl dark:border-white/[0.2] dark:bg-black dark:shadow-none",
-        className,
+        "row-span-1 relative overflow-hidden rounded-3xl border border-white/[0.1] group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4",
+        className
       )}
       style={{background: "rgba(4,7,29)", backgroundColor: "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)"}}
     >
@@ -52,7 +54,7 @@ export const BentoGridItem = ({
       <div className={`${id === 6 && 'flex justify-center h-full'}`}>
         <div className="w-full h-full absolute">
           {img && (
-            <Image alt={img} src={img} width={100} height={100} className={cn(imgClassName, 'object-cover, object-center')} />
+            <Image alt={img} src={img} width={50} height={50} className={cn(imgClassName, 'object-cover, object-center')} />
           )}
         </div>
         <div className={`${id === 5 && 'w-full opacity-80'} absolute right-0 -bottom-5`}>
@@ -63,23 +65,38 @@ export const BentoGridItem = ({
 
           {id === 6 && (
             <BackgroundGradientAnimation>
-              <div className="absolute z-50 flex items-center justify-center text-white font-bold"></div>
+              <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl"></div>
             </BackgroundGradientAnimation>
           )}
 
-          <div className={cn(titleClassName, 'group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-400 flex flex-col px-5 p-5 lg:p-10')}>
+          <div className={cn(titleClassName, 'group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10')}>
             <div className="font-sans font-extralight dark:text-[#c1c2d3] text-sm md:text-xs lg:text-base z-10 text-[#c1c2d3]">{description}</div>
             <div className="font-sans font-bold text-lg dark:text-[#c1c2d3] lg:text-3xl z-10 max-w-96 text-[#c1c2d3]">
               {title}
             </div>
-          </div>
-          
+          {id === 2 && <GridGlobe /> }
+          {id === 3 && (
+            <div className="flex gap-1 lg:gap-5 top-0 w-fit absolute right-1">
+              <div className="flex flex-col gap-3 ">
+                {['React.js', 'Next.js', 'Typescript'].map(
+                  (item) => (
+                    <span key={item} className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">{item}</span>
+                  )
+                )}
+                <span className="py-4 px-3 rounded-lg text-center bg-[#10132E]" />
+              </div>
+              <div className="flex flex-col gap-3 ">
+                <span className="py-4 px-3 rounded-lg text-center bg-[#10132E]" />
+                {['Tailwindcss', "Next Auth", "Redux toolkit"].map(
+                  (item) => (
+                    <span key={item} className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">{item}</span>
+                  )
+                )}
+              </div>
+            </div>
+          )}
       </div>
-      
-      <div className="transition duration-200 group-hover/bento:translate-x-2">
-        
-       
-      </div>
+    </div>
     </div>
   );
 };
