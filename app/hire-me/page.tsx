c9aca2920b91598c544e617b089b022500448778
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-11-15 20:30:00 +0500

    done

--- app/hire-me/page.tsx
+++ app/hire-me/page.tsx
@@ -26,20 +26,22 @@ const page = () => {
 
   return (
     <>
-    <section className='px-5 lg:px-20 my-5 mx-auto w-full flex flex-col justify-center relative overflow-hidden'>
+    <section className='px-5 max-w-7xl lg:px-20 my-5 mx-auto w-full flex flex-col justify-center relative overflow-hidden'>
         <div className='w-full flex justify-center items-center h-[300px] md:h-[600px] lg:h-full overflow-hidden '>
             <Image priority={true} src={"/profileImage (5).jpg"} alt="ProfileImage" className='object-cover lg:h-[630px] h-[500px] -z-10' width={1400} height={630}/>
             <Link href={"#contact"} className="absolute top-0 right-21">
               <MagicButton title='Hire me' icon={<FaArrowDown />} position='right' />
             </Link>
         </div>
-        <div className='absolute -bottom-10 left-0 flex justify-center items-center'>
+        <div className='absolute max-w-7xl -bottom-10 left-0 flex justify-center items-center'>
             <HireMeInfinite items={"HIRE ME"} direction='right' pauseOnHover={false} speed={"fast"} />
         </div>
     </section>
+    <section className="max-w-[1400px] mx-auto">
         <h1 className='text-[30px] sm:text-[40px] md:text-[50px] lg:text-[90px] font-bold text-center'>I Don’t Just Build Websites, I Shape Digital Journeys.</h1>
         <ThreeDMarqueeDemo />
         <Contact />
+      </section>
       </>
   )
 }
